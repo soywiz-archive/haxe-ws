c43e5fbd70259f0eab1a8d8ec559f4552e906f5c
tecteun <tecteun@hotmail.com>
2017-09-23 00:23:22 +0200

    Initial version for wss support

--- src/haxe/net/WebSocketServer.hx
+++ src/haxe/net/WebSocketServer.hx
@@ -3,37 +3,81 @@ import haxe.io.Error;
 import haxe.net.impl.SocketSys;
 import haxe.net.impl.WebSocketGeneric;
 import sys.net.Host;
-import sys.net.Socket;
+import sys.ssl.Socket;
+import sys.ssl.Certificate;
+import sys.ssl.Key;
 
 class WebSocketServer { 
 
 	var _isDebug:Bool;
-	var _listenSocket:Socket;
-	
-	function new(host:String, port:Int, maxConnections:Int, isDebug:Bool) {
+	var _isSecure:Bool;
+	var _listenSocket:sys.net.Socket;
+	#if neko
+	var keepalive:Dynamic;
+	#end
+	function new(host:String, port:Int, maxConnections:Int, isSecure:Dynamic = true, isDebug:Bool = false) {
 		_isDebug = isDebug;
-		_listenSocket = new Socket();
-		_listenSocket.setBlocking(false);
+		_isSecure = isSecure == true;
+		_listenSocket = isSecure ? new sys.ssl.Socket() : new sys.net.Socket() ;
+		
+		if(_isSecure){
+			cast(_listenSocket, sys.ssl.Socket).setCA( Certificate.loadFile("certificate/cert/root.crt") );
+        	cast(_listenSocket, sys.ssl.Socket).setCertificate( Certificate.loadFile("certificate/cert/localhost.crt"), Key.readPEM(sys.io.File.getContent("certificate/cert/localhost.key"), false) );
+			cast(_listenSocket, sys.ssl.Socket).verifyCert = false;
+		}
+		
 		_listenSocket.bind(new Host(host), port);
+		_listenSocket.setBlocking(false);
 		_listenSocket.listen(maxConnections);
+		
+		#if neko
+		keepalive = neko.Lib.load("std", "socket_set_keepalive",4);
+		#end
 	}
 	
-	public static function create(host:String, port:Int, maxConnections:Int, isDebug:Bool) {
-		return new WebSocketServer(host, port, maxConnections, isDebug);
+	public static function create(host:String, port:Int, maxConnections:Int, isSecure:Bool, isDebug:Bool) {
+		return new WebSocketServer(host, port, maxConnections, isSecure, isDebug);
 	}
 	
 	public function accept():WebSocket {
 		try {
-			var socket = _listenSocket.accept();
+			var socket:Dynamic = null;
+			 if(_isSecure){
+				var sslsocket:sys.ssl.Socket = cast(_listenSocket, sys.ssl.Socket).accept();
+				while(true){
+					try{
+						#if neko
+							keepalive( @:privateAccess sslsocket.__s, true, 60, 5 );
+						#end
+						sslsocket.waitForRead();
+						sslsocket.handshake();
+						break;
+					}catch(e:Dynamic){ 
+						switch (Std.string(e)) {
+							case "Blocked": continue;
+							case "SSL - No client certification received from the client, but required by the authentication mode": sslsocket.output.flush(); continue; //fix for chrome
+							case "X509 - Certificate verification failed, e.g. CRL, CA or signature check failed": break;
+							default:
+								if(_isDebug){
+									trace("Closing -> " + sslsocket.peer());
+									trace(Date.now() + " " + e);
+								}
+								sslsocket.close();
+								
+								break;
+						}
+					}
+				}
+				sslsocket.output.flush();
+				socket = sslsocket;
+			}else{
+				socket = _listenSocket.accept();
+			}
 			return WebSocket.createFromAcceptedSocket(Socket2.createFromExistingSocket(socket, _isDebug), '', _isDebug);
 		}
 		catch (e:Dynamic) {
-			if (e == 'Blocking' || e == Error.Blocked) {
-				return null;
-			}
-			else {
-				throw(e);
-			}
+			
+			return null;
 		}
 	}
 	

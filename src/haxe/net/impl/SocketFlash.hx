31f3605dd0ba927e0fe9558cde97a76734ef600b
Roman Mikhailov <mikhailovr@yandex.by>
2017-10-11 08:02:30 +0300

    Add subscription on `SecurityErrorEvent` for flash sockets. (#26)

--- src/haxe/net/impl/SocketFlash.hx
+++ src/haxe/net/impl/SocketFlash.hx
@@ -5,6 +5,7 @@ import haxe.io.Bytes;
 import flash.utils.ByteArray;
 import flash.events.ProgressEvent;
 import flash.events.IOErrorEvent;
+import flash.events.SecurityErrorEvent;
 import flash.events.Event;
 import flash.utils.Endian;
 import flash.net.Socket;
@@ -31,6 +32,10 @@ class SocketFlash extends Socket2 {
         });
         this.impl.addEventListener(flash.events.IOErrorEvent.IO_ERROR, function(e:IOErrorEvent) {
             if (debug) trace('SocketFlash.io_error');
+            this.onerror();
+        });
+        this.impl.addEventListener(flash.events.SecurityErrorEvent.SECURITY_ERROR, function(e:SecurityErrorEvent) {
+            if (debug) trace('SocketFlash.securityError');
             this.onerror();
         });
         this.impl.addEventListener(flash.events.ProgressEvent.SOCKET_DATA, function(e:ProgressEvent) {
